7944eebaa29d99481f5a9392a9a0ee0d85cf95d3
gernest <geofreyernest@live.com>
2018-06-26 15:37:38 +0300

    add TestVerifyClientAffinity

--- voltdbclient/client_affinity_test.go
+++ voltdbclient/client_affinity_test.go
@@ -2,6 +2,7 @@ package voltdbclient
 
 import (
 	"context"
+	"database/sql"
 	"database/sql/driver"
 	"io/ioutil"
 	"testing"
@@ -142,4 +143,87 @@ func TestClientAffinity(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestVerifyClientAffinity(t *testing.T) {
+	servers := "localhost:21212,localhost:21222,localhost:21232"
+	conn, err := OpenConn(servers)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	_, err = conn.Exec("@AdHoc", []driver.Value{"delete from customer"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = conn.Exec("add_customer", []driver.Value{
+		int64(1), "john", "doe",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	p, err := getProcedureStats(servers, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range p {
+		if v.Procedure == "add_customer" {
+			if v.PartitionID != 2 {
+				t.Errorf("expected partition id to be 2 got %d instead", v.PartitionID)
+			}
+		}
+	}
+}
+
+type procedureStat struct {
+	TimeSstamp       int64
+	HostID           int
+	HostName         string
+	SiteID           int
+	PartitionID      int
+	Procedure        string
+	Invocations      int
+	TimedInvocations int
+	MinExTime        int64
+	MaxExTime        int64
+	AvgMaxExTime     int64
+	MinResultSize    int
+	MaxResultSize    int
+	AvgResultSize    int
+	MinParamSetSize  int
+	MaxParamSetSize  int
+	AvgParamSetSize  int
+	Aborts           int
+	Failure          int
+	Transactional    int
+}
+
+func getProcedureStats(servers string, idx int64) ([]procedureStat, error) {
+	conn, err := sql.Open("voltdb", servers)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	rows, err := conn.Query("@Statistics",
+		"PROCEDURE", idx,
+	)
+	if err != nil {
+		return nil, err
+	}
+	var o []procedureStat
+	defer rows.Close()
+	for rows.Next() {
+		var p procedureStat
+		err := rows.Scan(&p.TimeSstamp, &p.HostID, &p.HostName, &p.SiteID, &p.PartitionID,
+			&p.Procedure, &p.Invocations, &p.TimedInvocations, &p.MinExTime, &p.MaxExTime,
+			&p.AvgMaxExTime, &p.MinResultSize, &p.MaxResultSize,
+			&p.AvgResultSize, &p.MinParamSetSize, &p.MaxParamSetSize, &p.AvgParamSetSize,
+			&p.Aborts, &p.Failure, &p.Transactional,
+		)
+		if err != nil {
+			return nil, err
+		}
+		o = append(o, p)
+	}
+	return o, nil
 }
