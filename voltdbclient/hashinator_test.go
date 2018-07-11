055a29758ead9581788e39acb4ea03c85518965e
gernest <geofreyernest@live.com>
2018-07-11 13:52:39 +0300

    sign fill the hash value from hashinator
    The refrence java code uses >>> which go doesnt have, we are using >> which will zero
    fill when the value is unsigned, by using signed int we are now having
    similar results to java.

--- voltdbclient/hashinator_test.go
+++ voltdbclient/hashinator_test.go
@@ -98,3 +98,25 @@ func BenchmarkHashinater_getHashedPartitionForParameter_Bytes(b *testing.B) {
 		}
 	}
 }
+
+func TestHashinator(t *testing.T) {
+	jsonBytes, err := ioutil.ReadFile("./test_resources/configBytes.bytes")
+	if err != nil {
+		t.Fatal(err)
+	}
+	h, err := newHashinatorElastic(JSONFormat, true, jsonBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sample := []int{2, 2, 1, 0, 1, 2, 2, 0, 0, 2}
+	for i := 0; i < 10; i++ {
+		partition, err := h.getHashedPartitionForParameter(0, int32(i))
+		if err != nil {
+			t.Fatal(err)
+		}
+		expect := sample[i]
+		if partition != expect {
+			t.Errorf("id: %d expected %d got %d", i, expect, partition)
+		}
+	}
+}
