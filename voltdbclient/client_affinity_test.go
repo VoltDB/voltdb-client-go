fb10172edbd82d040a99d4c384081d5582bd33dd
gernest <geofreyernest@live.com>
2018-07-20 13:57:53 +0300

    cleanup TestClientAffinity
    - rely on AFFINITY_SERVERS to pickup  cluster server
    - don't generate a topology file
    -  remove hashinator tests, these are already covered

--- voltdbclient/client_affinity_test.go
+++ voltdbclient/client_affinity_test.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
@@ -13,7 +12,10 @@ import (
 )
 
 func TestClientAffinity(t *testing.T) {
-	servers := "localhost:21212,localhost:21222,localhost:21232"
+	servers := os.Getenv("AFFINITY_SERVERS")
+	if servers == "" {
+		t.Skip("AFFINITY_SERVERS is not set")
+	}
 	conn, err := OpenConn(servers)
 	if err != nil {
 		t.Fatal(err)
@@ -54,97 +56,6 @@ func TestClientAffinity(t *testing.T) {
 		r.Close()
 		if conn.PartitionDetails == nil {
 			t.Error("expected partition details to be set")
-		}
-		b, err := conn.PartitionDetails.Dump()
-		if err != nil {
-			ts.Fatal(err)
-		}
-		err = ioutil.WriteFile("topology.json", b, 0600)
-		if err != nil {
-			ts.Error(err)
-		}
-	})
-	t.Run("must pick the right master", func(ts *testing.T) {
-		sample :=
-			[]struct {
-				query string
-				args  []driver.Value
-				hash  int
-				conn  string
-			}{
-				{
-					query: "Vote",
-					args: []driver.Value{
-						int64(4127351526),
-						int32(5),
-						int64(2),
-					},
-					hash: 0,
-					conn: "localhost:21222",
-				},
-				{
-					query: "Vote",
-					args: []driver.Value{
-						int64(2295722013),
-						int32(2),
-						int64(2),
-					},
-					hash: 4,
-					conn: "localhost:21232",
-				},
-				{
-					query: "Vote",
-					args: []driver.Value{
-						int64(5621008000),
-						int32(5),
-						int64(2),
-					},
-					hash: 0,
-					conn: "localhost:21222",
-				},
-				{
-					query: "Vote",
-					args: []driver.Value{
-						int64(2105510900),
-						int32(5),
-						int64(2),
-					},
-					hash: 4,
-					conn: "localhost:21232",
-				},
-				{
-					query: "Vote",
-					args: []driver.Value{
-						int64(2105510900),
-						int32(5),
-						int64(2),
-					},
-					hash: 4,
-					conn: "localhost:21232",
-				},
-				{
-					query: "Vote",
-					args: []driver.Value{
-						int64(7088159255),
-						int32(1),
-						int64(2),
-					},
-					hash: 2,
-					conn: "localhost:21222",
-				},
-			}
-		for _, v := range sample {
-			c, err := conn.getConnByCA(conn.PartitionDetails, v.query, v.args)
-			if err != nil {
-				ts.Fatal(err)
-			}
-			if c != nil {
-				if c.connInfo != v.conn {
-					t.Errorf("expected %s got %s", v.conn, c.connInfo)
-				}
-			} else {
-				t.Error("expected connection")
-			}
 		}
 	})
 }
