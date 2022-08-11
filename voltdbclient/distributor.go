f98f496ca2482512dc3d575f39088a808036e8ae
Richard Dykiel <rdykiel@voltdb.com>
2022-08-11 14:31:51 -0400

    ENG-22805: ensure correct validation of server certificate (#83)
    * ENG-22805: ensure correct validation of server certificate

--- voltdbclient/distributor.go
+++ voltdbclient/distributor.go
@@ -1,5 +1,5 @@
 /* This file is part of VoltDB.
- * Copyright (C) 2008-2018 VoltDB Inc.
+ * Copyright (C) 2008-2022 Volt Active Data Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -160,6 +160,11 @@ func OpenTLSConn(ci string, clientConfig ClientConfig) (*Conn, error) {
 		return nil, ErrMissingServerArgument
 	}
 	cis := strings.Split(ci, ",")
+	if clientConfig.TLSConfig == nil {
+		clientConfig.TLSConfig = &tls.Config{
+			InsecureSkipVerify: clientConfig.InsecureSkipVerify,
+		}
+	}
 	return newTLSConn(cis, clientConfig)
 }
 
